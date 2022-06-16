76e73bd5aceaaed249677dc616b7919434c77bcd
abhimanyu121993 <softpromani@gmail.com>
2022-06-17 00:41:50 +0530

    aqar out

--- resources/views/Frontend/includes/sidebar/iqac_menu.blade.php
+++ resources/views/Frontend/includes/sidebar/iqac_menu.blade.php
@@ -8,12 +8,13 @@
     <a href="#menu3" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false" > <span class="d-none d-md-inline">NAAC Accreditation</span> </a>
     <div class="collapse" id="menu3" data-parent="#sidebar"  >
         <a href="{{route('iqacSsr')}}" class="list-group-item">SSR 2020 </a>
-        <a href="{{route('aqar')}}" class="list-group-item">AQAR </a>
+        {{-- <a href="{{route('aqar')}}" class="list-group-item">AQAR </a> --}}
         <a href="#" class="list-group-item">Academic & Administrative Audit </a>
         <a href="#" class="list-group-item">NAAC Certificate Cycle-I </a>
         <a href="#" class="list-group-item">NAAC Certificate Cycle-II </a>
         <a href="#" class="list-group-item">NAAC PPT </a>
     </div>
+    <a href="{{route('aqar')}}" class="list-group-item d-inline-block collapsed sidenav "> <span class="d-none d-md-inline">AQAR</span> </a>
     <a href="{{route('iqacFeedback')}}" class="list-group-item d-inline-block collapsed sidenav "> <span class="d-none d-md-inline">Feedback</span> </a>
     <a href="{{route('iqacEvents')}}" class="list-group-item d-inline-block collapsed" > <span class="d-none d-md-inline">IQAC Events</span> </a>
     <a href="{{route('iqacReports')}}" class="list-group-item d-inline-block collapsed" > <span class="d-none d-md-inline">Reports</span> </a>
