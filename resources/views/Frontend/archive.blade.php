9f63877342899ba6135299e6da25a08e148970ee
KillerHeman <sharmahcool5@gmail.com>
2022-06-20 12:23:34 +0530

    himanshu frontend

--- resources/views/Frontend/archive.blade.php
+++ resources/views/Frontend/archive.blade.php
@@ -1,22 +1,26 @@
 @extends('Frontend.includes.layout')
 @section('content')
-    <section id="yt_spotlight1" class="block mb30">
+    <!-- slider starts here -->
+    <section id="yt_spotlight1" class="block">
         <div class="container">
             <div class="row">
                 <div id="slideshow" class="col-sm-12">
                     <div id="sj_k2slickslider346" class="sj-k2slickslider slide slickslider-right" data-interval="5000"
                         data-pause="hover">
                         <!-- Carousel items -->
-                        {{-- <h1 class="sliderText" style=" z-index:99;position:absolute;margin-left:40%;margin-top:6%;">
-                            Contact Us</h1> --}}
-                        <div class="text-effect" style=" z-index:99;position:absolute;margin-left:35%;margin-top:4%;">
-                            <span>Archieve</span>
-                        </div>
-
                         <div class="slickslider-items bg-style1">
                             <div class="slickslider-item item clearfix active">
-                                <div class="images-slideshow"><img alt="" src="{{ asset('Frontend/images/header.jpg') }}">
+                                <div class="images-slideshow"><img alt=""
+                                        src="{{ asset('Frontend/images/internal%20banners/banner119.jpg') }}" /></div>
+                                <div class="item-content">
+                                    <div class="item-content-inner">
+                                        <h5 class="item-title">
+                                            <a href="#" style="color:#7C0000" title="">Home</a> | <a
+                                                href="#" style="color:#7C0000" title="">Archive</a>
+                                            </h3>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
@@ -24,49 +28,58 @@
             </div>
         </div>
     </section>
+    <div class="container-fluid"
+        style="background-color: #00aeef;color:white;font-size:25px;font-weight:bold;padding:10px;margin-bottom:40px;">
+        <p class="text-center">Archive</p>
+    </div>
+    <!-- slider ends here -->
 
-<section class="container">
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingTwo">
-                <h4 class="panel-title">
-                    <a class="collapsed" role="button" data-toggle="collapse"
-                        data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
-                        aria-controls="collapseTwo">
-                        Archive
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
-                aria-labelledby="headingTwo">
-                <div class="panel-body">
-                    <ul>
-                        <li><a href="#">Regarding Reschedule of East Zone Inter University, Table- Tennis (W) Tournament 2021-22</a></li>
-                        <li><a href="#">Fixture For East Zone Inter University Table Tennis (W) tournament 2021-22</a></li>
-                        <li><a href="#">Admission 2021-23 Two year Full Time Master of Business Administration Programmemeu</a></li>
-                        <li><a href="#">LIST OF CANDIDATES FOUND ELIGIBLE FOR INTERVIEW IN PHYSICS (CLICK HERE)</a></li>
-                        <li><a href="#">Notice About Interview Letter of Physics (CLICK HERE)</a></li>
-                        <li><a href="#">Notice Interview Letter of Botany (CLICK HERE)</a></li>
-                    </ul>
+    <section class="container">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                            href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Archive
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        <ul>
+                            <li><a href="#">Regarding Reschedule of East Zone Inter University, Table- Tennis (W)
+                                    Tournament 2021-22</a></li>
+                            <li><a href="#">Fixture For East Zone Inter University Table Tennis (W) tournament
+                                    2021-22</a></li>
+                            <li><a href="#">Admission 2021-23 Two year Full Time Master of Business Administration
+                                    Programmemeu</a></li>
+                            <li><a href="#">LIST OF CANDIDATES FOUND ELIGIBLE FOR INTERVIEW IN PHYSICS (CLICK
+                                    HERE)</a></li>
+                            <li><a href="#">Notice About Interview Letter of Physics (CLICK HERE)</a></li>
+                            <li><a href="#">Notice Interview Letter of Botany (CLICK HERE)</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section class="container">
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="panel-body">
-            <ul>
-                <li><a href="#">Regarding Reschedule of East Zone Inter University, Table- Tennis (W) Tournament 2021-22</a></li>
-                <li><a href="#">Fixture For East Zone Inter University Table Tennis (W) tournament 2021-22</a></li>
-                <li><a href="#">Admission 2021-23 Two year Full Time Master of Business Administration Programmemeu</a></li>
-                <li><a href="#">LIST OF CANDIDATES FOUND ELIGIBLE FOR INTERVIEW IN PHYSICS (CLICK HERE)</a></li>
-                <li><a href="#">Notice About Interview Letter of Physics (CLICK HERE)</a></li>
-                <li><a href="#">Notice Interview Letter of Botany (CLICK HERE)</a></li>
-            </ul>
+    <section class="container">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel-body">
+                <ul>
+                    <li><a href="#">Regarding Reschedule of East Zone Inter University, Table- Tennis (W) Tournament
+                            2021-22</a></li>
+                    <li><a href="#">Fixture For East Zone Inter University Table Tennis (W) tournament 2021-22</a>
+                    </li>
+                    <li><a href="#">Admission 2021-23 Two year Full Time Master of Business Administration
+                            Programmemeu</a></li>
+                    <li><a href="#">LIST OF CANDIDATES FOUND ELIGIBLE FOR INTERVIEW IN PHYSICS (CLICK HERE)</a></li>
+                    <li><a href="#">Notice About Interview Letter of Physics (CLICK HERE)</a></li>
+                    <li><a href="#">Notice Interview Letter of Botany (CLICK HERE)</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</section>
-
+    </section>
 @endsection
--- resources/views/Frontend/course.blade.php
+++ resources/views/Frontend/course.blade.php
@@ -1,31 +1,35 @@
 @extends('Frontend.includes.layout')
 @section('content')
 
-    <section id="yt_spotlight1" class="block mb30">
+       <!-- slider starts here -->
+       <section id="yt_spotlight1" class="block">
         <div class="container">
             <div class="row">
-                <div id="slideshow" class="col-sm-12">
-                    <div id="sj_k2slickslider346" class="sj-k2slickslider slide slickslider-right" data-interval="5000"
-                        data-pause="hover">
-                        <!-- Carousel items -->
-                        {{-- <h1 class="sliderText" style=" z-index:99;position:absolute;margin-left:40%;margin-top:6%;">
-                            Contact Us</h1> --}}
-                        <div class="text-effect" style=" z-index:99;position:absolute;margin-left:35%;margin-top:4%;">
-                            <span>Programme/Course</span>
-                        </div>
-
-                        <div class="slickslider-items bg-style1">
-                            <div class="slickslider-item item clearfix active">
-                                <div class="images-slideshow"><img alt="" src="{{ asset('Frontend/images/header.jpg') }}">
+            <div id="slideshow" class="col-sm-12">
+                <div id="sj_k2slickslider346" class="sj-k2slickslider slide slickslider-right" data-interval="5000" data-pause="hover">
+                    <!-- Carousel items -->
+                    <div class="slickslider-items bg-style1">
+                        <div class="slickslider-item item clearfix active">
+                            <div class="images-slideshow"><img alt="" src="{{asset('Frontend/images/internal%20banners/banner119.jpg')}}"/></div>
+                            <div class="item-content">
+                                <div class="item-content-inner">
+                                    <h5 class="item-title" >
+                                        <a href="#" style="color:#7C0000" title="" >Home</a> | <a href="#" style="color:#7C0000" title="" >Courses</a>
+                                    </h3>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
             </div>
+            </div>
         </div>
     </section>
-
+    <div class="container-fluid" style="background-color: #00aeef;color:white;font-size:25px;font-weight:bold;padding:10px;">
+    <p class="text-center">Programme/Courses</p>
+    </div>
+    <!-- slider ends here -->
     <section class="container " style="margin-top: 50px;">
         <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
             <div class="panel panel-default">
