6f19c975eea40194e1cf1569d7a3aee06f5b2cc5
KillerHeman <sharmahcool5@gmail.com>
2022-06-21 12:59:57 +0530

    final commit

--- resources/views/Frontend/includes/sidebar/iqac_menu.blade.php
+++ resources/views/Frontend/includes/sidebar/iqac_menu.blade.php
@@ -4,6 +4,8 @@
     <a href="{{route('iqacStrategicPlan')}}" class="list-group-item d-inline-block collapsed" > <span class="d-none d-md-inline">Strategic Plan</span> </a>
     <a href="{{route('iqacVision')}}" class="list-group-item d-inline-block collapsed"> <span class="d-none d-md-inline">Vision, Mission & Strategies
     </span> </a>
+    <a href="#" class="list-group-item d-inline-block collapsed"> <span class="d-none d-md-inline">Composition of IQAC
+    </span> </a>
     <a href="{{route('iqacSsr')}}" class="list-group-item d-inline-block collapsed" > <span class="d-none d-md-inline">SSS</span> </a>
     <a href="#menu3" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false" > <span class="d-none d-md-inline">NAAC Accreditation</span> </a>
     <div class="collapse" id="menu3" data-parent="#sidebar"  >
@@ -12,7 +14,6 @@
         <a href="#" class="list-group-item">NAAC Certificate Cycle-II </a>
         <a href="#" class="list-group-item">NAAC Certificate Cycle-III </a>
         <a href="#" class="list-group-item">Certificates & Grade Card</a>
-        <a href="#" class="list-group-item">Composition of IQAC</a>
         <a href="#" class="list-group-item">Minutes of meeting </a>
         <a href="#" class="list-group-item">NAAC Guidelines</a>
         <a href="#" class="list-group-item">NAAC PPT </a>
