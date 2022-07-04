f4f14231e7cad5a9f61817f504457e00b4a864c9
KillerHeman <sharmahcool5@gmail.com>
2022-07-04 11:19:31 +0530

    rti page

--- resources/views/Frontend/rti_act.blade.php
+++ resources/views/Frontend/rti_act.blade.php
@@ -26,14 +26,173 @@
         </div>
     </div>
 </section>
-<div class="container-fluid" style="background-color: #00aeef;color:white;font-size:25px;font-weight:bold;padding:10px;">
-<p class="text-center">RTI ACT</p>
-</div>
 <!-- slider ends here -->
 
 
-    <section class="container" style="margin-top: 50px;margin-bottom:50px;">
-        <a href="#" style="font-size: 20px;color:black;">Data will be uploaded soon....</a>
-    </section>
+<div class="container-fluid" style="margin-top: 40px;" >
+    <div class="row">
+        <div class=" col-md-12">
+            <div class="panel">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col col-sm-5 col-xs-12">
+                            <h4 class="title ">Right To Information</span></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-body table-responsive">
+                    <table class="table table-hover" border="2" style="text-align: left;">
+                        <thead  >
+                            <tr >
+                                <th  style="font-size:15px;">S.No.</th>
+                                <th style="font-size:15px;">Title / Heading</th>
+                                <th style="font-size:15px;">Published</th>
+                                <th style="font-size:15px;">File</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Particulars of organization, functions and duties</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>Powers and duties of officers and employees</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td>Procedure followed in decision making</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>4.</td>
+                                <td>Norms for the discharge of functions</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>5.</td>
+                                <td>Rules, regulations, instructions, manuals and records held by it or under its control or used by its employees for discharging functions</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>6.</td>
+                                <td>A statement of the categories of documents that are held by it or under its control</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>7.</td>
+                                <td>Particulars of any arrangement that exists for consultation with, or representation by, members of the public in relation to the formation of its policy or implementation thereof</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>8.</td>
+                                <td>Statement of the boards, councils, committees and other bodies consisting of two or more persons constituted as its part or for the purpose of its advice, and as to whether meetings of those boards, councils, committees and other bodies are open to the public, or the minutes of such meetings are accessible for the public</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>9.</td>
+                                <td>Directory of its officers and employees</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>10.</td>
+                                <td>Monthly remuneration received by each of its officers and employees, including the system of compensation as provided in its regulations</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>11.</td>
+                                <td>Budget allocated to each of its agencies, indicating the particulars of all plans proposed, expenditure and reports of disbursement made</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>12.</td>
+                                <td>Manner of execution of subsidy programme, including amounts allocated and details of beneficiaries of such programme</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>13.</td>
+                                <td>Particulars of recipients of concession, permits or authorizations granted by it</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>14.</td>
+                                <td>Details in respect of information available to or held by it, reduced in an electronic form</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>15.</td>
+                                <td>Particulars of facilities available to citizens for obtaining information</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>16.</td>
+                                <td>Names, designations and other particulars of Public Information Officer (P.I.O.) / First Appellate Authority (F.A.A.)</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/pdf-icon.png') }}"
+                                    alt=""></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>17.</td>
+                                <td>Organization Chart</td>
+                                <td>12/07/2022</td>
+                                <td><a href="#"><img src="{{ asset('Frontend/images/links-icon.jpeg') }}"
+                                    alt="" height="30"></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
