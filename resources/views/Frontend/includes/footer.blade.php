f936c447cd1fd59448a824f64f7671c97ccdacc6
KillerHeman <sharmahcool5@gmail.com>
2022-07-13 14:53:55 +0530

    hisu lnmu update

--- resources/views/Frontend/includes/footer.blade.php
+++ resources/views/Frontend/includes/footer.blade.php
@@ -1,10 +1,50 @@
 <footer id="yt_footer" class="block">
+    <div class="container-fluid" style="background-color: grey">
+        <div class="row">
+            <div class="col-md-6 col-sm-12" >
+
+                <span style="color:white !important;font-size:15px;float: left;font-weight:bold;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Visitors No.
+                    <img src='https://www.counter12.com/img-YYBcA3bydabYy6Zy-77.gif' border='0'
+                        alt='LNMU Visitor Counter'>
+                </span>
+            </div>
+            <div class="col-md-6 col-sm-12" >
+                <span style="color:white !important;font-size:15px;float: right;">
+                    <b>
+                        <script language="Javascript">
+                            var today = new Date();
+                            var dd = today.getDate();
+
+                            var mm = today.getMonth() + 1;
+                            var yyyy = today.getFullYear();
+                            if (dd < 10) {
+                                dd = '0' + dd;
+                            }
+
+                            if (mm < 10) {
+                                mm = '0' + mm;
+                            }
+                            today = mm + '-' + dd + '-' + yyyy;
+                            console.log(today);
+                            today = mm + '/' + dd + '/' + yyyy;
+                            console.log(today);
+                            today = dd + '-' + mm + '-' + yyyy;
+                            console.log(today);
+                            today = dd + '/' + mm + '/' + yyyy;
+                            console.log(today);
+                            document.write("Last Updated on: " + today + "");
+                        </script>
+                    </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </span>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
-            <div id="yt_copyright" class="col-sm-12">
+            <div id="yt_copyright" class="col-sm-10">
                 <div class="copyright">
-                  
-                    <!--| <a href="#">Apply Online</a>--><br>Design & Developed By <b> IT CELL LNMU</b>&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;Copyright &copy; 2022 LNMU. All Rights Reserved
+                    <!--| <a href="#">Apply Online</a>--><br>Design & Developed By <b> IT CELL
+                        LNMU</b>&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;Copyright &copy; 2022 LNMU. All Rights Reserved
                 </div>
             </div>
         </div>
